7526d26aaa3baba712eb387ac33f4b95b80d4106
JasonSubMara <jie.yr127@gmail.com>
2019-12-25 11:35:12 +0800

    first commit

---
+++ linelip.js
@@ -0,0 +1,27 @@
+/**
+ * @description Multiline text overflow ellipsis
+ * @param {Object} line.
+ * @param {Object} element.
+ */
+function linelip(lineNum, tagele) {
+	var eles = document.getElementsByClassName(tagele);
+	var lineNum = lineNum || 2;	
+	for (var i = 0; i<eles.length; i++) {
+		var element = eles[i]
+		var text = element.textContent;
+		var totalTextLen = element.textContent.length;
+		var baseWidth = window.getComputedStyle(element).width; 
+		var baseFontSize = window.getComputedStyle(element).fontSize;
+		var lineWidth = +baseWidth.slice(0,-2);
+		var strNum = Math.floor(lineWidth/ +baseFontSize.slice(0,-2));
+		var content = "";
+		var totalStrNum = Math.floor(strNum * lineNum);
+		var lastIndex = totalStrNum - totalTextLen;
+		if (totalTextLen > totalStrNum) {
+			content = text.slice(0, lastIndex - 1).concat("...");
+		}else {
+			content = text;
+		}
+		element.innerHTML = content;
+	}
+}	
